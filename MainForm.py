a5db978e8a6d23c571fd2d8d73d63369ce00f620
Traky <dengk7@mcmaster.ca>
2020-10-15 04:30:34 -0400

    comment organization

--- MainForm.py
+++ MainForm.py
@@ -1,21 +1,33 @@
 from tkinter import *
 import mysql.connector
 
+# connect to database
 cnx = mysql.connector.connect(user='user1',
                               password='password',
                               host='127.0.0.1',
                               database='RVH')
+
+# main cursor
 cursor = cnx.cursor()
+
+# cursor created for bound checks
 cursor_row_count = cnx.cursor()
 
-window = Tk()
-window.title("Patient Lookup")
-window.geometry('600x250')
-# window.resizable(False, False)
+# get the number of patients by fetching all
+cursor_row_count.execute('SELECT * FROM RVH.Patients')
+cursor_row_count.fetchall()
+max_row = cursor_row_count.rowcount
 
+# UI
+window = Tk()
+window.title("RVH Patient Lookup")
+window.geometry('600x250')
+
+# frame for the three labels under patient number
 top_frame = Frame(window)
 top_frame.grid(column=1, row=1, rowspan=3)
 
+# search key
 Label(window,
       text='Patient Number',
       anchor="e",
@@ -24,11 +36,12 @@ Label(window,
 Patient_Num = Entry(window, width=15)
 Patient_Num.grid(row=0, column=1)
 
+# Patient Name
 Label(window, text='Patient Name', anchor="e", width=15,
       font="Arial 14 bold").grid(row=1, column=0)
 Label(top_frame, width=15, text='_________________________').grid(row=0,
                                                                   column=0)
-
+# Care Centre Name
 Label(window,
       text='Care Centre Name',
       anchor="e",
@@ -36,7 +49,7 @@ Label(window,
       font="Arial 14 bold").grid(row=2, column=0)
 Label(top_frame, width=15, text='_________________________').grid(row=1,
                                                                   column=0)
-
+# Nurse in Charge
 Label(window,
       text='Nurse in Charge',
       anchor="e",
@@ -44,57 +57,58 @@ Label(window,
       font="Arial 14 bold").grid(row=3, column=0)
 Label(top_frame, width=15, text='_________________________').grid(row=2,
                                                                   column=0)
-
-# blank
+# blank row for a visual break
 Label(window, text='').grid(row=4, columnspan=4)
 
-# ---- horizontal ----
-# frame as a container for treatment entires
-Treatment_frame = Frame(window)
-Treatment_frame.grid(row=6, columnspan=4)
+# ---- horizontal treatment record(s) ----
+# frame for label widgets
+treatment_frame = Frame(window)
+treatment_frame.grid(row=6, columnspan=4)
 
+# Treatment ID
 Label(window, text='Treatment ID', width=15,
       font="Arial 14 bold").grid(row=5, column=0)
-Label(Treatment_frame, width=15, text='___________________').grid(row=1,
+Label(treatment_frame, width=15, text='___________________').grid(row=1,
                                                                   column=0)
-
+# Treatment Name
 Label(window, text='Treatment Name', width=15,
       font="Arial 14 bold").grid(row=5, column=1)
-Label(Treatment_frame, width=15, text='___________________').grid(row=1,
+Label(treatment_frame, width=15, text='___________________').grid(row=1,
                                                                   column=1)
-
+# Physician ID
 Label(window, text='Physician ID', width=15,
       font="Arial 14 bold").grid(row=5, column=2)
-Label(Treatment_frame, width=15, text='___________________').grid(row=1,
+Label(treatment_frame, width=15, text='___________________').grid(row=1,
                                                                   column=2)
-
+# Date
 Label(window, text='Date', width=15, font="Arial 14 bold").grid(row=5,
                                                                 column=3)
-Label(Treatment_frame, width=15, text='___________________').grid(row=1,
+Label(treatment_frame, width=15, text='___________________').grid(row=1,
                                                                   column=3)
 
-# error message place holder
+# Error message place holder
 errorMsg = Label(window, text='', foreground='red')
 errorMsg.grid(row=0, column=3)
 
-# get the number of patients by fetching all
-cursor_row_count.execute('SELECT * FROM RVH.Patients')
-cursor_row_count.fetchall()
-max_row = cursor_row_count.rowcount
-
 
 def searchPatient():
+    """This function is called when the user clicks on the button to loop up a patient. 
+    It queries the database and populate the relevant labels in top_frame and treatment_frame.
 
-    # clear
+    Some patients have no care centres because they are outpatients. 
+    Some patients have no treatment records. 
+    """
+    # clear previous info
     for widget in top_frame.winfo_children():
         widget.destroy()
-    for widget in Treatment_frame.winfo_children():
+    for widget in treatment_frame.winfo_children():
         widget.destroy()
     errorMsg.config(text='')
 
+    # get user input
     pid = Patient_Num.get()
 
-    # validate entry; prevent sql injection
+    # validate entry for an integer
     try:
         int(pid)
         pass
@@ -122,7 +136,8 @@ def searchPatient():
 
     for row in cursor:
         row_num += 1
-        # only update name if the input box is empty
+        # update the top_frame labels only once
+        # since top_frame to treatment_frame is one-to-many
         if row_num == 1:
             Label(top_frame, width=15, text=row[1]).grid(row=1, column=1)
             if row[2] != None:
@@ -133,17 +148,19 @@ def searchPatient():
                 Label(top_frame, width=15, text=row[3]).grid(row=3, column=1)
             else:
                 Label(top_frame, width=15, text='').grid(row=3, column=1)
+        # update treatment_frame
         if row[4] != None:
-            Label(Treatment_frame, width=15, text=row[4]).grid(row=row_num + 1,
+            Label(treatment_frame, width=15, text=row[4]).grid(row=row_num + 1,
                                                                column=0)
-            Label(Treatment_frame, width=15, text=row[5]).grid(row=row_num + 1,
+            Label(treatment_frame, width=15, text=row[5]).grid(row=row_num + 1,
                                                                column=1)
-            Label(Treatment_frame, width=15, text=row[6]).grid(row=row_num + 1,
+            Label(treatment_frame, width=15, text=row[6]).grid(row=row_num + 1,
                                                                column=2)
-            Label(Treatment_frame, width=15, text=row[7]).grid(row=row_num + 1,
+            Label(treatment_frame, width=15, text=row[7]).grid(row=row_num + 1,
                                                                column=3)
 
 
+# Button
 Button(window, text="Look Up Patient", padx=10, pady=6,
        command=searchPatient).grid(row=0, column=2)
 
